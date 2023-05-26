945ed4c5262b62863b209e5cdc5375cafb8ec9d3
Aravindth-007 <118426062+Aravindth-007@users.noreply.github.com>
2023-05-26 14:38:21 +0530

    chore: email function completed

--- Assets/JS/contact_us.js
+++ Assets/JS/contact_us.js
@@ -1,26 +1,55 @@
+// let send = document.getElementById("send_id");
+// send.addEventListener("click", function (s) {
+//     s.preventDefault();
 
-let send = document.getElementById("send_id");
-send.addEventListener("click", function (s) {
-    s.preventDefault();
+//     let fr_name = document.getElementById("cont_name").value;
+//     let fr_email = document.getElementById("cont_email").value;
+//     let fr_subject = document.getElementById("cont_subject").value;
+//     let fr_message = document.getElementById("cont_msg").value;
 
-    let fr_name = document.getElementById("cont_name").value;
-    let fr_email = document.getElementById("cont_email").value;
-    let fr_subject = document.getElementById("cont_subject").value;
-    let fr_message = document.getElementById("cont_msg").value;
+//     // required details for send email.
+//     let details = {from_name: fr_name,
+//                    from_email:fr_email,
+//                    from_subject:fr_subject,
+//                    to_email:"kaviprakash594@gmail.com",
+//                    message:`Hi i am ${fr_name}This is my message ${fr_message} Thank you.`}
+//                 //    email send function
 
-    // required details for send email.
-    let details = {from_name: fr_name,
-                   from_email:fr_email,
-                   from_subject:fr_subject,
-                   to_email:"kaviprakash594@gmail.com",
-                   message:`Hi i am ${fr_name}This is my message ${fr_message} Thank you.`}
-                //    email send function
+//                 emailjs.send("service_sp6m68","template_uo2qcla",details)
+//                 .then((res) => {
+//                     alert("We will contact you soon");
+//                     })
+//                     .catch((err) =>{
+//                         console.log(err);
+//                     })
+// });
 
-                emailjs.send("service_sp6m68","template_uo2qcla",details)
-                .then((res) => {
-                    alert("We will contact you soon");
-                    })
-                    .catch((err) =>{
-                        console.log(err);
-                    })
+const form = document.getElementById("emailForm");
+console.log(form);
+form.addEventListener("click", function (event) {
+  event.preventDefault();
 });
+function sende() {
+  const name = document.getElementById("cont_name").value;
+  const email = document.getElementById("cont_email").value;
+  const subject = document.getElementById("cont_subject").value;
+  const message = document.getElementById("cont_msg").value;
+  const body =
+    "Name : " +
+    name +
+    "<br /> Email : " +
+    email +
+    "<br /> Subject : " +
+    subject +
+    "<br /> Message : " +
+    message;
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "kaviprakash594@gmail.com",
+    Password: "D822D3CB3EF5FCB9D45AEAA7E84252D5707B",
+    To: "kaviprakash594@gmail.com",
+    From: "kaviprakash594@gmail.com",
+    Subject: subject,
+    Body: body,
+  }).then(alert("Message sent successfully!"));
+}
